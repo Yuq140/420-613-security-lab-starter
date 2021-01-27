cc63deaf2f0912c006ad399c72fb676461424dc9
Yuq140 <alaire3@gmail.com>
2021-01-27 15:04:53 -0500

    Added index action handler

--- SecurityLab1_Starter/Controllers/ErrorController.cs
+++ SecurityLab1_Starter/Controllers/ErrorController.cs
@@ -6,6 +6,10 @@ using System.Web.Mvc;
 
 namespace SecurityLab1_Starter.Controllers {
     public class ErrorController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
         // GET: Error
         public ActionResult NotFound() {
             return View();
